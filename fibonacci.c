a2c90c5be1d7ddaf453300e46c57adb121df0e34
RubanZargar <rubanzargar56@gmail.com>
2025-10-11 23:40:52 +0530

    Add files via upload

---
+++ fibonacci.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int fibonacci(int num) {
+    if (num == 0)
+    return 0;  
+    else if (num == 1)
+    return 1;  
+    else
+    return fibonacci(num - 1) + fibonacci(num - 2);  
+}
+int main() {
+    int num;
+
+    printf("Enter the number of terms upto which you wanna generate the series:\n ");
+    scanf("%d", &num);
+
+    printf("Fibonacci Series up to %d terms:\n", num);
+    for (int i = 0; i < num; i++) {
+
+    printf("%d ", fibonacci(i));
+
+    }
+
+    printf("\n");
+    return 0;
+}
+
+ 
+
+
+ 
